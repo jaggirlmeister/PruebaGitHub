cfa7286fb55e97952ec21267a1ce66c5ad0b6c1f
Nayla <nayla.mar2@gmail.com>
2020-03-19 13:45:19 -0300

    Clase 1 - Repaso AJAX - Repositorio de perfiles
    Clase 1. Repaso de AJAX. Creación de Repositorio de perfiles con imagen y nombre de usuario.

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,1 +1,53 @@
-alert("Está levantando el JS!!");
+$( document ).ready(function() {
+
+
+    $("#handle_ajax").click (function(){
+        $.ajax({
+            url: 'https://reqres.in/api/users',
+            success: function(response){
+                console.log(response.data);
+                var listadoUsuarios = $('#listado');
+    
+                $.each(response.data, function(index, elemento) { 
+                    console.log(elemento.first_name);
+                    listadoUsuarios.append(
+                        "<div class='card'><h2>"
+                        + elemento.first_name + 
+                        "</h2><img src ='"+elemento.avatar+"'</div>");
+                });
+            },
+            error: function(e){
+                console.log("Hubo un error, shit!", e);
+            },
+            beforeSend: function () { $("#loader").show(); console.log('buscando data')},           
+            complete: function () { $("#loader").hide(); console.log('data buscada!') }
+        });
+    });
+
+    $("#handle_ajax_delete").click(function(){
+        console.log("click en delete")
+        $("#listado").html("");
+    })
+
+});
+
+
+/* 
+$('#listado').append('<li>Usuario 1</li>');
+$('#listado').append('<li>Usuario 2</li>');
+$('#listado').append('<li>Usuario 3</li>');
+$('#listado').append('<li>Usuario 4</li>');
+$('#listado').append('<li>Usuario 5</li>');
+ */
+
+/*    
+var mi_var = "holis"
+console.log( "ready!", mi_var ); 
+*/
+
+
+/* 
+function pepe ();
+
+const pepe = () => {};  
+*/
