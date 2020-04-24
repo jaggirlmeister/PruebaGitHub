9f9232e6eddcef477bee5507e59e85eb88b7f7d9
Nayla <nayla.mar2@gmail.com>
2020-04-23 23:39:09 -0300

    Bermuda triangle! Trazado de Coordenadas con polygons

--- Clase4/assets/js/main.js
+++ Clase4/assets/js/main.js
@@ -1,5 +1,7 @@
 let markersAll = [];
 let map = "";
+
+
 window.initMap = () => {
   // The location of Palermo Hollywood
   const initialLocation = { lat: -34.581870, lng: -58.433660 };
@@ -18,6 +20,26 @@ window.initMap = () => {
       mapTypeIds: [],
     },
   });
+
+  //Bermuda Triangle Polygon. It has to end where it starts
+  var triangleCoords = [
+    {lat: 25.774, lng: -80.190},
+    {lat: 18.466, lng: -66.118},
+    {lat: 32.321, lng: -64.757},
+    {lat: 25.774, lng: -80.190}
+  ];
+
+  // Construct the polygon.
+  var bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+  bermudaTriangle.setMap(map);
+
   fetchMarkers(map);
 };
 
